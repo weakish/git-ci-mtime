a3fb2c7901a8acf2c29dd31fe33dfcfd6ccc6eb8
Jakukyo Friel <weakish@gmail.com>
2016-09-04 13:04:31 +0800

    Support git options and multiple files.

--- git-ci-mtime.sh
+++ git-ci-mtime.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+
+git_ci_mtime() {
+    modificationTime=$(ls --full-time "$fileToCommit" | cut -d ' ' -f 6-8)
+    git add "$fileToCommit"
+    git commit --date "$modificationTime" "$@" "$fileToCommit"
+}
+
 fileToCommit="$1"
-modificationTime=$(ls --full-time "$fileToCommit" | cut -d ' ' -f 6-8)
-git add "$fileToCommit"
-git commit --date "$modificationTime" "$fileToCommit"
+shift 1
+git_ci_mtime "$@"
